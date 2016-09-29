3d63ca033e80e82ee083877f84c82b6ed89ebdc2
s4923233 <s4923233@bournemouth.ac.uk>
2016-09-29 11:43:27 +0100

    Initial commit and test

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+
+/*
+int main(){
+    using namespace std;
+    cout<<"hello world"<<endl;
+    return 0; 
+}
+
+*/
+int main(){
+    std::cout<<"hello world\n";
+    return EXIT_SUCCESS;
+}
